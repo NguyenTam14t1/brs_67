8f49a6f8e4537ba1da92ea4f72eff3cec07baa70
nguyentam14t1@gmail.com <nguyentam14t1@gmail.com>
2018-08-02 08:22:37 +0700

    design_static_pages

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,40 @@
+<footer class="footer-area section-gap">
+    <div class="container">
+        <div class="row">
+            
+            <div class="col-lg-3 col-md-6 single-footer-widget">
+                <h4>{{ trans('lang.top_books') }}</h4>
+                <ul>
+                    <li><a href="#">{{ trans('lang.top_book_first') }}</a></li>
+                </ul>
+            </div>
+            
+            <div class="col-lg-3 col-md-6 single-footer-widget">
+                <h4>{{ trans('lang.categories') }}</h4>
+                <ul>
+                    <li><a href="#">{{ trans('lang.categories') }}</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-6 single-footer-widget">
+                <h4>{{ trans('lang.home') }}</h4>
+                <h4>{{ trans('lang.contact') }}</h4>
+            </div>
+            <div class="col-lg-3 col-md-6 single-footer-widget">
+                <div class="footer-single-widget">
+                    <h4>{{ trans('lang.subscribe') }}</h4>
+                    {{ Form::open(['method' => 'POST', 'route' => '']) }}
+                        {{ Form::email('email', null, ['id' => 'eemail', 'placeholder' => trans('plh_email')]) }}
+                        {{ Form::button('<i class="fa fa-arrow-right"></i>') }}
+                    {{ Form::close() }}
+                </div>
+            </div>
+            
+        </div>
+        <div class="footer-bottom row align-items-center">
+            <p class="footer-text m-0 col-lg-8 col-md-8">
+                {{ trans('lang.copyright') }}
+            </p>
+            
+        </div>
+    </div>
+</footer>
---
+++ resources/views/layouts/right_bar.blade.php
@@ -0,0 +1,66 @@
+<div class="col-lg-4">
+    <div class="sidebars-area">
+        <div class="single-sidebar-widget editors-pick-widget">
+            <h6 class="title">{{ trans('lang.top_books') }}</h6>
+            <div class="editors-pick-post">
+                <div class="feature-img-wrap relative">
+                    <div class="feature-img relative">
+                        <div class="overlay overlay-bg"></div>
+                        {{ HTML::image('books/img/e1.jpg', ['class' => 'img-fluid']) }}
+                    </div>
+                    <ul class="tags">
+                        <li><a href="#">{{ trans('lang.parent_category') }}</a></li>
+                    </ul>
+                </div>
+                <div class="details">
+                    <a href="image-post.html">
+                        <h4 class="mt-20">A Discount Toner Cartridge Is
+                        Better Than Ever.</h4>
+                    </a>
+                    <ul class="meta">
+                        <li><a href="#"><span class="lnr lnr-user"></span>Mark wiens</a></li>
+                        <li><a href="#"><span class="lnr lnr-calendar-full"></span>03 April, 2018</a></li>
+                        <li><a href="#"><span class="lnr lnr-bubble"></span>06 </a></li>
+                    </ul>
+                    <p class="excert">
+                        Lorem ipsum dolor sit amet, consecteturadip isicing elit, sed do eiusmod tempor incididunt ed do eius.
+                    </p>
+                </div>
+                <div class="post-lists">
+                    <div class="single-post d-flex flex-row">
+                        <div class="thumb">
+                            {{ HTML::image('books/img/e2.jpg', ['class' => 'img-fluid']) }}]
+                        </div>
+                        <div class="detail">
+                            <a href="image-post.html"><h6>Help Finding Information
+                            Online is so easy</h6></a>
+                            <ul class="meta">
+                                <li><a href="#"><span class="lnr lnr-calendar-full"></span>03 April, 2018</a></li>
+                                <li><a href="#"><span class="lnr lnr-bubble"></span>06</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="single-sidebar-widget most-popular-widget">
+            <h6 class="title">{{ trans('lang.most_popular') }}</h6>
+            <div class="single-list flex-row d-flex">
+                <div class="thumb">
+                    {{ HTML::image('books/img/m1.jpg', ['class' => 'img-fluid']) }}
+                </div>
+                <div class="details">
+                    <a href="image-post.html">
+                        <h6>Help Finding Information
+                        Online is so easy</h6>
+                    </a>
+                    <ul class="meta">
+                        <li><a href="#"><span class="lnr lnr-calendar-full"></span>03 April, 2018</a></li>
+                        <li><a href="#"><span class="lnr lnr-bubble"></span>06</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
